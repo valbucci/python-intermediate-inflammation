11ede53a118b45559bfa7eb84bda0ebc55dad710
valbucci <valbuxvb@gmail.com>
2024-11-26 15:46:19 +0000

    Fixed horizontal spacing

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row
+contains inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
